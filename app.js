b731d6f78ab558d53711a944d0a4510bd00051c9
CaroA10 <candreap2004@gmail.com>
2025-02-06 17:18:09 -0500

    Crear la función actualizarListaAmigos

--- app.js
+++ app.js
@@ -8,5 +8,19 @@ function agregarAmigo () {
         alert('"Por favor, inserte un nombre."');
     }
     document.querySelector('#amigo').value='';
+    document.getElementById('amigo').focus();
+    actulizarListaAmigos ();
     console.log(amigos);
 }
+
+function actulizarListaAmigos () {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML="";
+    for (let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos [i];
+        lista.appendChild(li);
+
+    }
+}
+
